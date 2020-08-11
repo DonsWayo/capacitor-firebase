cd6c15fe1f37b047bbb34260f0ebbf0a87863770
DonsWayo <djwayomix@gmail.com>
2020-08-11 13:55:43 +0200

    addes ios basic cloud messaging methods

--- packages/cloud-messaging/ios/Plugin/Plugin.m
+++ packages/cloud-messaging/ios/Plugin/Plugin.m
@@ -4,5 +4,9 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(FirebaseCloudMessaging, "FirebaseCloudMessaging",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(initFirebaseMessaging, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(didRegisterWithToken, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(subscribeToTopic, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(unsubscribeFrom, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getToken, CAPPluginReturnPromise);
 )
--- packages/cloud-messaging/ios/Plugin/Plugin.swift
+++ packages/cloud-messaging/ios/Plugin/Plugin.swift
@@ -1,17 +1,79 @@
 import Foundation
 import Capacitor
+import FirebaseMessaging
+import FirebaseInstanceID
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(FirebaseCloudMessaging)
-public class FirebaseCloudMessaging: CAPPlugin {
-
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
+public class FirebaseCloudMessaging: CAPPlugin, MessagingDelegate {
+    
+    @objc func initFirebaseMessaging(_ call: CAPPluginCall) {
+        Messaging.messaging().delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(self.didRegisterWithToken(notification:)), name: Notification.Name(CAPNotifications.DidRegisterForRemoteNotificationsWithDeviceToken.name()), object: nil)
+        
         call.success([
-            "value": value
+            "success": true
         ])
     }
+    
+    @objc func didRegisterWithToken(notification: NSNotification) {
+        guard let deviceToken = notification.object as? Data else {
+            return
+        }
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    @objc func subscribeToTopic(_ call: CAPPluginCall) {
+        let topicName = call.getString("topic") ?? nil
+        
+        if topicName != nil {
+            call.error("No topic provided")
+            return
+        }
+        Messaging.messaging().subscribe(toTopic: "weather") { error in
+            if error != nil {
+                call.error("Error to subscribe to topic \(String(describing: topicName))")
+            } else {
+                call.success([
+                    "success": "subscribed to topic \(String(describing: topicName))"
+                ])
+            }
+        }
+        
+    }
+    
+    @objc func unsubscribeFrom(_ call: CAPPluginCall) {
+        let topicName = call.getString("topic") ?? nil
+        
+        if topicName != nil {
+            call.error("No topic provided")
+            return
+        }
+        
+        Messaging.messaging().unsubscribe(fromTopic: topicName!) { error in
+            if error != nil {
+                call.error("Can't unsubscribe from topic \(String(describing: topicName))")
+            } else {
+                call.success([
+                    "success": "unsubscribed from topic \(String(describing: topicName))"
+                ])
+            }
+        }
+    }
+    
+    @objc func getToken(_ call: CAPPluginCall) {
+           InstanceID.instanceID().instanceID { result, error in
+               if error != nil {
+                   call.error("Failed to get instance FirebaseID", error)
+               } else {
+                   call.success([
+                    "token": result!.token
+                   ]);
+               }
+           }
+       }
+    
 }
