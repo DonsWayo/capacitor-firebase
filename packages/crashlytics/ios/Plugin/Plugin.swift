90a2a7139b031ff18cf9e98e18fa3e6b14c0e517
DonsWayo <djwayomix@gmail.com>
2020-08-16 19:34:27 +0200

    added crashlytics ios side

--- packages/crashlytics/ios/Plugin/Plugin.swift
+++ packages/crashlytics/ios/Plugin/Plugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import FirebaseCrashlytics
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -7,11 +8,55 @@ import Capacitor
  */
 @objc(FirebaseCrashlytics)
 public class FirebaseCrashlytics: CAPPlugin {
-
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.success([
-            "value": value
-        ])
+    
+    @objc func testCrash(_ call: CAPPluginCall) {
+        call.success()
+        fatalError()
+    }
+    
+    @objc func setCustomValue(_ call: CAPPluginCall) {
+        let options = call.getObject("options") ?? nil
+        
+        if options == nil {
+            call.error("customValue is missing")
+            return
+        }
+        
+        let forKey = options!["forKey"] ?? nil
+        let value = options!["value"] ?? nil
+        
+        if forKey == nil  || value == nil {
+            call.error("forKey or value is missing")
+        }
+        Crashlytics.crashlytics().setCustomValue(value as Any, forKey: forKey as! String)
+        
+        call.success()
+    }
+    
+    @objc func setUserID(_ call: CAPPluginCall) {
+        let userId = call.getString("userId") ?? nil
+        
+        if userId == nil {
+            call.error("userId is missing")
+            return
+        }
+        
+        Crashlytics.crashlytics().setUserID(userId!)
+        call.success()
+        
+    }
+    
+    @objc func logMessage(_ call: CAPPluginCall) {
+        let message = call.getString("message") ?? nil
+        
+        if message == nil {
+            call.error("message is missing")
+            return
+        }
+        
+        Crashlytics.crashlytics().log(message!)
+        
+        call.success()
+        
     }
 }
