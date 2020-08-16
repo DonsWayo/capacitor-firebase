45e1cb82f478abe56e26c1e61fe788f1d4901a75
DonsWayo <djwayomix@gmail.com>
2020-08-16 20:01:04 +0200

    crashlytics add android methods

--- packages/crashlytics/android/src/main/java/com/donswayo/capacitor/firebase/crashlytics/FirebaseCrashlytics.java
+++ packages/crashlytics/android/src/main/java/com/donswayo/capacitor/firebase/crashlytics/FirebaseCrashlytics.java
@@ -6,15 +6,88 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
 @NativePlugin
 public class FirebaseCrashlytics extends Plugin {
 
-    @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    private com.google.firebase.crashlytics.FirebaseCrashlytics mFirebaseCrashlytics;
 
-        JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+    @PluginMethod
+    public void initCrashlytics(PluginCall call) {
+        mFirebaseCrashlytics = com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance();
+        call.success();
     }
+
+    @PluginMethod
+    public void testCrash(PluginCall call) {
+        call.success();
+        throw new RuntimeException("Test Crash"); // Force a crash
+    }
+
+    @PluginMethod
+    public void logMessage(PluginCall call) {
+        String message = call.getString("message");
+
+        if (message == null) {
+            call.error("Message is missing!");
+            return;
+        }
+
+        mFirebaseCrashlytics.log(message);
+        call.success();
+    }
+
+    @PluginMethod
+    public void setCustomValue(PluginCall call) throws JSONException {
+        JSObject options = call.getObject("options");
+        String type = call.getString("type");
+
+        if (options == null) {
+            call.error("options are missing!");
+            return;
+        }
+
+        if(type == null ) {
+            type = "string";
+        }
+
+        switch (type) {
+            case "int":
+                mFirebaseCrashlytics.setCustomKey(options.getString("forKey"), options.getInt("value"));
+                break;
+            case "boolean":
+                mFirebaseCrashlytics.setCustomKey(options.getString("forKey"), options.getBoolean("value"));
+                break;
+            case "double":
+                mFirebaseCrashlytics.setCustomKey(options.getString("forKey"), options.getDouble("value"));
+                break;
+            case "long":
+                mFirebaseCrashlytics.setCustomKey(options.getString("forKey"), options.getLong("value"));
+                break;
+            default:
+                mFirebaseCrashlytics.setCustomKey(options.getString("forKey"), options.getString("value"));
+                break;
+
+        }
+
+        call.success();
+    }
+
+    @PluginMethod
+    public void setUserId(PluginCall call) {
+        String userId = call.getString("userId");
+
+        if (userId == null) {
+            call.error("UserId is missing!");
+            return;
+        }
+
+        mFirebaseCrashlytics.setUserId("12345");
+        call.success();
+    }
+
 }
