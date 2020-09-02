1511122302cdda82182729870b240b07cce47e27
DonsWayo <djwayomix@gmail.com>
2020-09-02 10:20:00 +0200

    update docs

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -34,19 +34,35 @@ module.exports = {
     lastUpdated: false,
     nav: [
       {
-        text: 'Guide',
+        text: 'Docs',
         link: '/guide/',
       },
       {
-        text: 'Config',
-        link: '/config/'
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
+        text: 'Github',
+        link: 'https://github.com/DonsWAYO/capacitor-firebase'
       }
     ],
     sidebar: [
+      {
+        title: 'Analytics',   // required
+        path: '/analytics/installation',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+        children: [
+          '/analytics/installation',
+          '/analytics/usage'
+        ]
+      },
+      {
+        title: 'Cloud Messaging',   // required
+        path: '/cloud-messaging/installation',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+        children: [
+          '/cloud-messaging/installation',
+          '/cloud-messaging/usage'
+        ]
+      },
       {
         title: 'Crashlytics',   // required
         path: '/crashlytics/installation',      // optional, link of the title, which should be an absolute path and must exist
@@ -55,6 +71,36 @@ module.exports = {
         children: [
           '/crashlytics/installation',
           '/crashlytics/usage'
+        ]
+      },
+      {
+        title: 'Firestore',   // required
+        path: '/firestore/installation',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+        children: [
+          '/firestore/installation',
+          '/firestore/usage'
+        ]
+      },
+      {
+        title: 'Performance Monitoring',   // required
+        path: '/performance-monitoring/installation',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+        children: [
+          '/performance-monitoring/installation',
+          '/performance-monitoring/usage'
+        ]
+      },
+      {
+        title: 'Storage',   // required
+        path: '/storage/installation',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+        children: [
+          '/storage/installation',
+          '/storage/usage'
         ]
       }
     ]
