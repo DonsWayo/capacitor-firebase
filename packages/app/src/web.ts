f731defbba88a889c0e6e798ccad072be19c0158
DonsWayo <jjcv-10@hotmail.com>
2021-01-13 23:47:29 +0000

    chore: refactor app plugin

--- packages/app/src/web.ts
+++ packages/app/src/web.ts
@@ -1,9 +1,6 @@
 import { WebPlugin } from '@capacitor/core';
 import { FirebaseAppPlugin, FirebaseInitConfig } from './definitions';
-
-const FIREBASECDN = "https://www.gstatic.com/firebasejs/7.17.2/firebase-app.js";
-
-declare var window: any;
+import firebase from "firebase/app";
 
 export class FirebaseAppWeb extends WebPlugin implements FirebaseAppPlugin {
 
@@ -17,47 +14,18 @@ export class FirebaseAppWeb extends WebPlugin implements FirebaseAppPlugin {
   }
 
   async initFirebase(config: FirebaseInitConfig): Promise<boolean> {
-    console.log('Init App')
     try {
-      await this.addFirebaseScript();
       return new Promise((resolve, reject) => {
-        if (this.isFirebaseInitialized()) {
+        if (firebase.apps.length > 1) {
           reject(false);
         }
-        this.app = window.firebase;
-        this.app.initializeApp(config);
+        firebase.initializeApp(config);
         resolve(true);
       });
     } catch (error) {
       console.log(error);
       return false;
     }
-  }
-
-  private isFirebaseInitialized() {
-    if (!window.firebase) {
-      return false;
-    }
-
-    const firebaseApps = window.firebase.apps;
-    if (firebaseApps && firebaseApps.length === 0) {
-      return false;
-    } else {
-      return true;
-    }
-
-  }
-
-  private addFirebaseScript(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      const element = window.document.querySelector("head");
-      const file = window.document.createElement("script");
-      file.type = "text/javascript";
-      file.src = FIREBASECDN;
-      file.onload = resolve;
-      file.onerror = reject;
-      element.appendChild(file);
-    });
   }
 }
 
