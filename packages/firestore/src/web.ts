c5d37ba533339d602a7ccde1bcb3a91286cdda36
DonsWayo <jjcv-10@hotmail.com>
2021-01-13 23:45:22 +0000

    chore: refactor firestore plugin

--- packages/firestore/src/web.ts
+++ packages/firestore/src/web.ts
@@ -1,13 +1,9 @@
 import { WebPlugin } from '@capacitor/core';
 import { FirebaseFirestorePlugin, Where, Order } from './definitions';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
-const FIREBASECDN = "https://www.gstatic.com/firebasejs/7.17.2/firebase-firestore.js";
-
-declare var window: any;
 export class FirebaseFirestoreWeb extends WebPlugin implements FirebaseFirestorePlugin {
-
-  firestore: any = null;
-
   constructor() {
     super({
       name: 'FirebaseFirestore',
@@ -22,7 +18,7 @@ export class FirebaseFirestoreWeb extends WebPlugin implements FirebaseFirestore
 
   async addDocument(options: { collection: string, document: Object }) {
       const { collection, document } = options;
-      const doc = await this.firestore.collection(collection).add(document);
+      const doc = await firebase.firestore().collection(collection).add(document);
       return doc;
   }
 
@@ -31,56 +27,32 @@ export class FirebaseFirestoreWeb extends WebPlugin implements FirebaseFirestore
       console.log(options);
       let docs: any = [];
       if(collection && !where && !order && !limit){
-        const getDocs = await this.firestore.collection(collection).get();
+        const getDocs = await firebase.firestore().collection(collection).get();
         getDocs.forEach(function(doc: any) {
           docs.push(doc.data());
         });
         return {"docs": docs}
       }
       if(collection && where && !order && !limit) {
-        return await this.firestore.collection(collection).where(where.value, where.compare, where.on).get();
+        return await firebase.firestore().collection(collection).where(where.value, where.compare, where.on).get();
       }
       if(collection && where && order && !limit) {
         if(order && order.descending) {
-          return await this.firestore.collection(collection).where(where.value, where.compare, where.on).orderBy(order.by, 'desc').get();
+          return await firebase.firestore().collection(collection).where(where.value, where.compare, where.on).orderBy(order.by, 'desc').get();
         } else {
-          return await this.firestore.collection(collection).where(where.value, where.compare, where.on).orderBy(order.by).get();
+          return await firebase.firestore().collection(collection).where(where.value, where.compare, where.on).orderBy(order.by).get();
         }
       }
       if(collection && where && limit && !order) {
-        return await this.firestore.collection(collection).where(where.value, where.compare, where.on).limit(limit).get();
+        return await firebase.firestore().collection(collection).where(where.value, where.compare, where.on).limit(limit).get();
       }
       if(collection && where && order && limit) {
         if(order && order.descending) {
-          return await this.firestore.collection(collection).where(where.value, where.compare, where.on).orderBy(order.by, 'desc').limit(limit).get();
+          return await firebase.firestore().collection(collection).where(where.value, where.compare, where.on).orderBy(order.by, 'desc').limit(limit).get();
         } else {
-          return await this.firestore.collection(collection).where(where.value, where.compare, where.on).orderBy(order.by).limit(limit).get();
+          return await firebase.firestore().collection(collection).where(where.value, where.compare, where.on).orderBy(order.by).limit(limit).get();
         }
       }
-  }
-
-  async initFirestore(): Promise<boolean | unknown> {
-    console.log('Init Analitycs')
-      return new Promise( async (resolve) => {
-        await this.addFirebaseScript();
-        this.firestore = window.firebase.firestore();
-        resolve(true);
-      })
-      .catch((error) => {
-        return error;
-      })
-  }
-
-  private addFirebaseScript(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      const element = window.document.querySelector("head");
-      const file = window.document.createElement("script");
-      file.type = "text/javascript";
-      file.src = FIREBASECDN;
-      file.onload = resolve;
-      file.onerror = reject;
-      element.appendChild(file);
-    });
   }
 }
 
